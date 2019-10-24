03bbf33ad68287c25b5b4456a675a9550d455ae2
Nikolas <artem.portno.39@mail.ru>
2019-10-24 20:15:02 +0300

    1.7.6. Android-практикум: рисование узоров на Canvas(линии)

---
+++ app/src/main/java/com/example/myapplication/MyView.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class MyView extends View {
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStrokeWidth(5);
+        int y = 0;
+        while (y < canvas.getHeight()) {
+            canvas.drawLine(0, y, this.getWidth(), y, paint);
+            y += 30;
+        }
+    }
+}
